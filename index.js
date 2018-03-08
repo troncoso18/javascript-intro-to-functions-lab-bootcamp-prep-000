ee2332136665a667dcef115cc1c3da34022262b4
Alfredo Troncoso <troncofut8@hotmail.com>
2018-03-08 18:11:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,6 @@
+function shout(string) {return string.toUpperCase()}
+function whisper(string) {return string.toLowerCase()}
+function logShout(string) {console.log('HELLO')}
+function logWhisper(string) {console.log("hello")}
+function sayHiToGrandma(Hi, Grandma) {return `I can't hear you!`}
+function sayHiToGrandma(I cant hear you) {console.log("YES INDEED!")}
